dfe1e0e90b0ed51488eac90f2904a8892aa8a1a7
BogdanBrizhaty <demaskinas@yandex.ua>
2017-11-02 01:33:56 +0200

    LogEventArgs and RuntimeInterpreterExceptionEventArgs added

---
+++ Interpreter/Interpreter/LogEventArgs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interpreter.Interpreter
+{
+    public class LogEventArgs : EventArgs
+    {
+        public string LogMessage { get; protected set; }
+        public DateTime LogDate { get; private set; }
+        public LogEventArgs(string msg)
+        {
+            LogDate = DateTime.Now;
+            LogMessage = msg;
+        }
+        public override string ToString()
+        {
+            return String.Format("{ at {0:dd/MM/yyyy H:mm:ss zzz} msg: {1} }", LogDate, LogMessage);
+        }
+    }
+}
---
+++ Interpreter/Interpreter/RuntimeInterpreterExceptionEventArgs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interpreter.Interpreter
+{
+
+    /// <summary>
+    /// Used for detailed info. In case of demo interpreter, fust inherit LogEventArgs
+    /// </summary>
+    public class RuntimeInterpreterExceptionEventArgs : LogEventArgs
+    {
+        public RuntimeInterpreterExceptionEventArgs(string msg) : base(msg)
+        {
+        }
+    }
+}
